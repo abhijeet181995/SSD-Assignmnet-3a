36c96da0a12841d629aacc52aad0788f82e2c8eb
Abhijeet Srivastava <abhijeet181995@gmail.com>
2020-11-05 15:17:08 +0530

    Added Q1 Q2 Q3 PartB and Employee Folder

--- q3.py
+++ q3.py
@@ -1,4 +1,5 @@
 import json
+import os
 def convertfinallist(free):
     finallist=[]
     flag0=0
@@ -27,17 +28,18 @@ def convertfinallist(free):
         finallist.append(slot[0]+'-'+slot[1])
     return finallist
 
-def output(free1,free2,duration,d,ans):
+def output(employee_names,list_free,duration,date,ans):
     with open("output.txt",'w') as of:
-        of.write("Employee1:")
-        of.write(json.dumps(convertfinallist(free1)))
-        of.write("\nEmployee2:")
-        of.write(json.dumps(convertfinallist(free2)))
+        for i in range(len(employee_names)):
+            of.write(employee_names[i])
+            of.write(":")
+            of.write(json.dumps(convertfinallist(list_free[i])))
+            of.write("\n")
         of.write("\nSlot Duration:%.1f hrs \n"%(duration))
         if len(ans)==0:
-            of.write(json.dumps({d:"No Available slots"}))
+            of.write(json.dumps({date:"No Available slots"}))
         else:
-            of.write(json.dumps({d:convertfinallist(ans)}))
+            of.write(json.dumps({date:convertfinallist(ans)}))
 
 
 def genratelist(start,end):
@@ -83,27 +85,56 @@ def process_slots(slotss):
     return freeslots
 
        
-alltimeslot=genratelist(900,1701) 
+alltimeslot=genratelist(900,1701)
 
-with open("Employee1.txt" , 'r') as e1:
-    dic1 = eval(e1.read())
+list_files = os.listdir("Employee")
+list_files_path = ["Employee/"+x for x in list_files]
+list_dic=[]
 
-with open("Employee2.txt" , 'r') as e2:
-    dic2 = eval(e2.read())
+for file in list_files_path:
+    with open(file , 'r') as e1:
+        dic = eval(e1.read())
+        list_dic.append(dic)
 
-if dic1['Employee1'].keys()==dic2['Employee2'].keys():
-    d = list(dic1['Employee1'].keys())[0]
-    free1 = process_slots(dic1['Employee1'][d])
-    free2 = process_slots(dic2['Employee2'][d])
-    free3 = [x for x in free1 if x in free2]
+
+employee_names = [x[:-4] for x in list_files]
+
+flag=0
+
+date1  = list(list_dic[0][employee_names[0]].keys())[0]
+date2 = list(list_dic[1][employee_names[1]].keys())[0]
+
+if date1!=date2:
+    flag=1
+
+free1 = process_slots(list_dic[0][employee_names[0]][date1])
+free2 = process_slots(list_dic[1][employee_names[1]][date2])
+free3 = [x for x in free1 if x in free2]
+
+list_free =[]
+list_free.append(combine(free1))
+list_free.append(combine(free2))
+
+
+for i in range(2,len(employee_names)):
+    currdate=list(list_dic[i][employee_names[i]].keys())[0]
+    if currdate!=date1:
+        flag=1
+    free = process_slots(list_dic[i][employee_names[i]][currdate])
+    list_free.append(combine(free))
+    free3 = [x for x in free if x in free3]
+
+ans=[]
+slotduration = float(input("Input The Slot Duration\n"))
+slotdurationmins = int(slotduration*60)
+for slot in combine(free3):
+    if alltimeslot.index(slot[0])+slotdurationmins<len(alltimeslot):
+        if alltimeslot[(alltimeslot.index(slot[0])+slotdurationmins)] <= slot[1]:
+            ans.append([slot[0],alltimeslot[(alltimeslot.index(slot[0])+slotdurationmins)]])
+            break
+
+if flag==1:
     ans=[]
-    slotduration = float(input("Input The Slot Duration\n"))
-    slotdurationmins = int(slotduration*60)
-    for slot in combine(free3):
-        if alltimeslot.index(slot[0])+slotdurationmins<len(alltimeslot):
-            if alltimeslot[(alltimeslot.index(slot[0])+slotdurationmins)] <= slot[1]:
-                ans.append([slot[0],alltimeslot[(alltimeslot.index(slot[0])+slotdurationmins)]])
-                break
-    output(combine(free1),combine(free2),slotduration,d,ans)
+    output(employee_names,list_free,slotduration,date1,ans)
 else:
-    print("No Slot Available")
+    output(employee_names,list_free,slotduration,date1,ans)
